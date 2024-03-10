acc8d1bcc8aeb53e271d2b440559eeaa1bc8c2f3
Nadeesha Nethmini <Nadeesha.N@CreativeSoftware.com>
2024-03-10 17:53:42 +0530

    search fnctinality for jobs

--- lib/views/screens/employerHome.dart
+++ lib/views/screens/employerHome.dart
@@ -58,12 +58,15 @@ class _EmployerHomeState extends State<EmployerHome> {
       'position': 'UI Designer',
       'jobtype': 'Part Time',
       'workplace': 'Hybrid',
-      'location': 'CodeSphere-Colombo,Sri Lanka',
+      'location': 'CodeSphere-Colombo,Canada',
       'salaryLower': 2000,
       'salaryUpper': 15000
 
     }
   ];
+
+  List<Map<String,dynamic>> filteredJobDetails =[];
+  bool onSearch = false;
 
 
   @override
@@ -86,11 +89,29 @@ class _EmployerHomeState extends State<EmployerHome> {
                     controller: searchController,
                     onChanged: (value){
                       setState(() {
-
+                        onSearch = true;
+                        // filteredJobDetails = jobDetails.where((element) => element['position'].toString().toLowerCase().contains(value.toLowerCase())).toList();
+                        //search value can be position,jobtype,workplace,location,salary lower,salary upper
+                        filteredJobDetails = jobDetails.where((element) =>
+                                             element['position'].toString().toLowerCase().contains(value.toLowerCase()) ||
+                                             element['jobtype'].toString().toLowerCase().contains(value.toLowerCase()) ||
+                                             element['workplace'].toString().toLowerCase().contains(value.toLowerCase()) ||
+                                             element['location'].toString().toLowerCase().contains(value.toLowerCase()) ||
+                                             element['salaryLower'].toString().toLowerCase()==(value.toLowerCase()) ||
+                                             element['salaryUpper'].toString().toLowerCase()==(value.toLowerCase())).toList();
                       });
                     },
                     decoration: InputDecoration(
-                      suffixIcon: Icon(Icons.search),
+                      suffixIcon: GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              searchController.clear();
+                              onSearch = false;
+                            });
+                          },
+                          child: Icon(Icons.remove)
+                      ),
+                      prefixIcon: Icon(Icons.search),
                       hintText: 'Search',
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.grey, width: 2.0),
@@ -109,6 +130,7 @@ class _EmployerHomeState extends State<EmployerHome> {
               }, MediaQuery.of(context).size.width * 0.8),
               SizedBox(height: 20,),
               AppFonts.customizeText('Recently Created', Colors.black, 14, FontWeight.bold),
+              onSearch==false?
               Expanded(
                 child:ListView.builder(
                   itemCount: jobDetails.length,
@@ -140,7 +162,43 @@ class _EmployerHomeState extends State<EmployerHome> {
                     );
                   },
                 ),
+              ):onSearch==true && !filteredJobDetails.isEmpty?
+              Expanded(
+                child:ListView.builder(
+                  itemCount: filteredJobDetails.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title:AppFonts.customizeText(filteredJobDetails[index]['position'], Colors.black, 14, FontWeight.bold),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AppFonts.customizeText(filteredJobDetails[index]['location'], Colors.black, 14, FontWeight.normal),
+                            Row(
+                              children: [
+                                AppFonts.customizeText(filteredJobDetails[index]['jobtype'], Colors.blue, 14, FontWeight.bold),
+                                SizedBox(width: 20,),
+                                AppFonts.customizeText(filteredJobDetails[index]['workplace'], Colors.blue, 14, FontWeight.bold),
+                              ],
+                            ),
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children:[
+                                  AppFonts.customizeText('LKR ${filteredJobDetails[index]['salaryLower']} - ${filteredJobDetails[index]['salaryUpper']}/Mo', Colors.green, 14, FontWeight.normal),
+                                ]
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ):
+              Expanded(
+                child:Center(child: AppFonts.customizeText('No Jobs Found', Colors.black, 14, FontWeight.normal)),
               )
+
             ],
           ),
         ),
